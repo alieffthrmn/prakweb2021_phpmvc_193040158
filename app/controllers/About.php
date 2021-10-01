3a74c7da32cbb721461191cb2dcb14b5449d53a0
Alief Fathurrohman <fathurrohmanalief@gmail.com>
2021-10-01 15:29:17 +0700

    menambah parameter umur di method index About.php #view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -7,8 +7,8 @@ class About
     echo 'About/page';
   }
 
-  public function index($nama = 'Alief Fathurrohman', $pekerjaan = 'mahasiswa')
+  public function index($nama = 'Alief Fathurrohman', $pekerjaan = 'mahasiswa', $umur = 19)
   {
-    echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+    echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan. Saya berumur $umur tahun.";
   }
 }
