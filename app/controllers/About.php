919123507ef48ed4df71c95f00f3fcfe50804d11
Alief Fathurrohman <fathurrohmanalief@gmail.com>
2021-10-01 15:47:35 +0700

    mengirimkan data ke view #view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -5,7 +5,10 @@ class About extends Controller
 
   public function index($nama = 'Alief Fathurrohman', $pekerjaan = 'mahasiswa', $umur = 19)
   {
-    $this->view('about/index');
+    $data['nama'] = $nama;
+    $data['pekerjaan'] = $pekerjaan;
+    $data['umur'] = $umur;
+    $this->view('about/index', $data);
   }
 
   public function page()
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -10,6 +10,8 @@
 
 <body>
   <h1>About Me</h1>
+  <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun,
+    saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
 </body>
 
 </html>
